e9bfe9b4633f5883fe3ac1748f5625a77a24f00f
maybejke <amrakitin@gmail.com>
2019-05-25 00:39:26 +0300

    1,2 +-3

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,21 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+from random import random, randint
+
+
+a = []
+N = int(random()*10)
+for i in range(N):
+    a.append(int(random()*100))
+print(N)
+print(a)
+
+for i in range(N):
+    f = True
+    for j in range(N):
+        if a[i] == a[j] and a != j:
+            f = False
+            break
+        if f == True:
+            print(a[i], end=' ')
