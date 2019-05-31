74b9bd4b2f6a59e0f2b6436e247a62be5d38ea0a
maybejke <amrakitin@gmail.com>
2019-05-31 18:22:27 +0200

    hm4 rakitin

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,55 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+# 5.	В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию (индекс) в массиве.
+
+
+import timeit
+import cProfile
+
+"""
+Напишите программу, доказывающую или проверяющую, что для множества
+натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
+ где n - любое натуральное число.
+"""
+
+
+
+N = 100000
+
+a = 5
+sum_el = 0
+
+
+def equal2():
+    n = 5
+    sum_el = 0
+    for i in range(n):
+        sum_el += i
+        if i == n:
+            if sum_el == n * (n + 1) / 2:
+                print('Teory is true')
+            else:
+                pass
+        else:
+            i += 1
+
+print(timeit.timeit('equal2()', setup="from __main__ import equal2", number=5))
+
+
+def equal1(n=1, start_el=1, sum_el=0):
+    sum_el += start_el
+    if start_el == n:
+        if sum_el == n * (n + 1) / 2:
+            print("Teory is true")
+        else:
+            pass
+    else:
+        start_el += 1
+        return equal1(n, start_el, sum_el)
+
+print(timeit.timeit('equal1(n=5, start_el=1, sum_el=0)', setup="from __main__ import equal1", number=5))
+cProfile.run('equal1()')
+cProfile.run('equal2()')
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,52 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+import timeit
+
+
+def sie(lim, n):
+    def sieve(n):
+        a = [x for x in range(n + 1)]
+        a[1] = 0
+        lst = []
+        i = 2
+        while i <= n:
+            if a[i] != 0:
+                lst.append(a[i])
+                for j in range(i, n + 1, i):
+                    a[j] = 0
+            i += 1
+        return lst
+    while len(sieve(n)) < lim:
+        n += 1
+    return sieve(n).pop()
+
+
+def nosieve(lim, n):
+    def nosieve_algo(n):
+        lst = []
+        for i in range(2, n + 1):
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                lst.append(i)
+        return lst
+    while len(nosieve_algo(n)) < lim:
+        n += 1
+    return nosieve_algo(n).pop()
+
+
+lim = int(input("Введите номер простого числа: "))
+n = 2
+
+
+print(sie(lim, n))
+print(nosieve(lim, n))
+
+print(timeit.timeit("sie(100, 2)", setup="from __main__ import sie", number=10))
+print(timeit.timeit("nosieve(100, 2)", setup="from __main__ import nosieve", number=10))
+
+
+# Решето работает быстрее на большем порядковом номере.
