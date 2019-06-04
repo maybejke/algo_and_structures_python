9534d69e84094f825973ed76edd5253530f6d479
maybejke <amrakitin@gmail.com>
2019-06-04 22:48:20 +0200

    hm6_rakitin

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,80 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+from memory_profiler import profile
+
+
+
+@profile
+def function():
+    data = {}
+    n = int(input("Введите количество предприятий: "))
+    p = 0
+    for i in range(n):
+        pname = input((str(i+1) + " Наименование предприятия: "))
+        p_1kv = int(input("Прибыль за первый квартал: "))
+        p_2kv = int(input("Прибыль за второй квартал: "))
+        p_3kv = int(input("Прибыль за трейтий квартал: "))
+        p_4kv = int(input("Прибыль за четвертый квартал: "))
+        # p_sred = (p_1kv + p_2kv + p_3kv + p_4kv)/4
+        p_god = (p_1kv + p_2kv + p_3kv + p_4kv)
+        data[pname] = p_god
+        p += p_god
+    avrg = p / n
+
+    print(f"Средняя прибыль за год  всех предприятий: {avrg}")
+    print(f"{data}")
+
+    for i in data:
+        if data[i] > avrg:
+            print(f"Предприятия прибыль больше среднего: {i}")
+    for i in data:
+        if data[i] < avrg:
+            print(f"Предприятия прибыль меньше среднего: {i}")
+
+function()
+
+@profile
+def sie(lim, n):
+    def sieve(n):
+        a = [x for x in range(n + 1)]
+        a[1] = 0
+        lst = []
+        i = 2
+        while i <= n:
+            if a[i] != 0:
+                lst.append(a[i])
+                for j in range(i, n + 1, i):
+                    a[j] = 0
+            i += 1
+        return lst
+    while len(sieve(n)) < lim:
+        n += 1
+    return sieve(n).pop()
+
+@profile
+def nosieve(lim, n):
+    def nosieve_algo(n):
+        lst = []
+        for i in range(2, n + 1):
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                lst.append(i)
+        return lst
+    while len(nosieve_algo(n)) < lim:
+        n += 1
+    return nosieve_algo(n).pop()
+
+
+lim = int(input("Введите номер простого числа: "))
+n = 2
+
+
+print(sie(lim, n))
+print(nosieve(lim, n))
+
+
+# так понимаю не большие объемы, забирает по 10 мб и успокаивается. macOS.
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,31 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+from pympler.asizeof import asizeof
+
+
+class Class1:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+class Class2:
+    __slots__ = ['name', 'surname']
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+a = Class1('Alexey', 'Rakitin')
+b = Class2('Alexey', 'Rakitin')
+
+print(a.__dict__)
+print(b.__slots__)
+print(asizeof(a))
+print(asizeof(b))
+
+# 392 используя слотс памятив  2 раза меньше.
+# 168
