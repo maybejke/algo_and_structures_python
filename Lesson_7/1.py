5f4ee0af15529421542b4120d1c432d4e908ab56
maybejke <amrakitin@gmail.com>
2019-06-09 22:26:33 +0300

    1_task prepare

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,21 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+from random import randint
+
+
+def selection_sort(array_to_sort):
+    a = array_to_sort
+    for i in range(len(a)):
+        idx_max = i
+        for j in range(i + 1, len(a)):
+            if a[j] > a[idx_max]:
+                idx_max = j
+        tmp = a[idx_max]
+        a[idx_max] = a[i]
+        a[i] = tmp
+    return a
+
+
+ary = [randint(-100, 100) for i in range(10)]
+print(selection_sort(ary))
